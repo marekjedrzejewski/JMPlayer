fb85f7b37fba11cd9d286660f5af6ccbc515661d
marek <mjedrzejewski92@gmail.com>
2015-12-01 12:26:21 +0100

    Add basic functionality to Track (get artist, get title)

--- app/src/main/java/jm/jmplayer/Track.java
+++ app/src/main/java/jm/jmplayer/Track.java
@@ -1,8 +1,10 @@
 package jm.jmplayer;
 
+import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 
 public class Track {
+    static MediaMetadataRetriever metaRetriever = new MediaMetadataRetriever();
     private Uri uri;
     private String title;
     private String artist;
@@ -11,13 +13,22 @@ public class Track {
     public Track(String path)
     {
         this.uri = Uri.parse(path);
+        metaRetriever.setDataSource(path);
+        title = metaRetriever.extractMetadata(
+                MediaMetadataRetriever.METADATA_KEY_TITLE);
+        artist = metaRetriever.extractMetadata(
+                MediaMetadataRetriever.METADATA_KEY_ARTIST);
+
     }
 
     public Uri getUri(){ return uri; }
 
-    public String getArtist(){ return "Darude"; }
+    public String getArtist(){
+        return artist;
 
-    public String getTitle() { return "Sandstorm"; }
+    }
+
+    public String getTitle() { return title; }
 
     public long getLengthInSeconds(){ return lengthInSeconds; }
 
