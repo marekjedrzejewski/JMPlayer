db418e700306072b4cc08e3942afaa6eb290c826
marek <mjedrzejewski92@gmail.com>
2015-12-01 13:23:46 +0100

    Add time getting functions to Track

--- app/src/main/java/jm/jmplayer/Track.java
+++ app/src/main/java/jm/jmplayer/Track.java
@@ -8,17 +8,27 @@ public class Track {
     private Uri uri;
     private String title;
     private String artist;
-    private long lengthInSeconds;
+    private long duration;
 
     public Track(String path)
     {
         this.uri = Uri.parse(path);
         metaRetriever.setDataSource(path);
+
         title = metaRetriever.extractMetadata(
                 MediaMetadataRetriever.METADATA_KEY_TITLE);
+        if (title == null)
+            title = uri.getLastPathSegment();
+
         artist = metaRetriever.extractMetadata(
                 MediaMetadataRetriever.METADATA_KEY_ARTIST);
+        if (artist == null)
+            artist = "N/A";
 
+        duration = Long.parseLong(
+                metaRetriever.extractMetadata(
+                        MediaMetadataRetriever.METADATA_KEY_DURATION)
+        );
     }
 
     public Uri getUri(){ return uri; }
@@ -30,8 +40,17 @@ public class Track {
 
     public String getTitle() { return title; }
 
-    public long getLengthInSeconds(){ return lengthInSeconds; }
+    public long getLengthInSeconds(){ return duration; }
 
-    public String getLengthAsString(){ return "06:66"; }
+    public String getLengthAsString(){
+        long durationS = duration / 1000;
+        long h = durationS / 3600;
+        long m = (durationS - h * 3600) / 60;
+        long s = durationS - (h * 3600 + m * 60);
+        if (h == 0)
+            return String.format("%d:%02d", m, s);
+        else
+            return String.format("%d:%02d:%02d", h, m, s);
+    }
 
 }
